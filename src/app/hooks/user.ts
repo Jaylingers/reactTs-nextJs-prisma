67a648252ced0a7b9412d8003ceea46e33272295
jaylingers <jomarie@ajio.co.jp>
2024-02-10 21:30:02 +0800

    data is displayed by table

--- src/app/hooks/user.ts
+++ src/app/hooks/user.ts
@@ -1,38 +1,13 @@
-import {Prisma, PrismaClient, users} from '@prisma/client';
-import { useEffect, useState } from "react";
 
+import {PrismaClient} from "@prisma/client";
 
 const prisma = new PrismaClient();
 
-export async function getUsers(): Promise<{
-    users?: users[];
-    error?: Prisma.PrismaClientKnownRequestError | unknown;
-}> {
+export const getUsers = async () => {
     try {
-        console.log("jay2")
         const users = await prisma.users.findMany();
-        console.log(users)
         return {users};
     } catch (error) {
-        // console.log("jay1")
         return { error: error instanceof Error ? error.message : 'Unknown error occurred' };
     }
 }
-
-export interface UserData extends users {}
-
-export const getAllUserData = () => {
-    const [users, setUsers] = useState<UserData[]>([]); // Initial state set to null
-    const getAllUserData = () => {
-        const fetchData = async () => {
-            const {users,error} = await getUsers();
-            console.log("Fetched users:", users);
-            console.log(error)
-            setUsers(users as UserData[]);
-        };
-        fetchData().then();
-    }
-
-    useEffect(getAllUserData, [users]); // Empty dependency array ensures that this effect runs only once
-    return { users };
-};
--- src/app/showUserUsingPrisma/List.tsx
+++ src/app/showUserUsingPrisma/List.tsx
@@ -1,27 +1,37 @@
 
-import {getAllUserData} from "@/app/hooks/user";
+import {getUsers} from "@/app/hooks/user";
+import {users} from ".prisma/client";
 
 interface ListProps {
 }
+export interface UserData extends users {}
+const List: React.FC<ListProps> = async (props) => {
 
-const List: React.FC<ListProps> = (props) => {
+    const queryOutput = await getUsers();
 
-    const {users} = getAllUserData();
+    return (
+        <main className="flex min-h-screen flex-col items-center justify-between p-24">
+            <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
+            <table>
+                <thead>
+                <tr>
+                    <th>Name</th>
+                    <th>Email</th>
+                </tr>
+                </thead>
+                <tbody>
+                {queryOutput.users?.map((user) => (
+                    <tr key={user.user_id}>
+                        <td>{user.username}</td>
+                        <td>{user.email}</td>
+                    </tr>
+                ))}
+                </tbody>
+            </table>
+            </div>
+        </main>
 
-  return (
-      <>
-          {
-              users?.map((user) => {
-                    return (
-                        <div key={user.user_id}>
-                            <p>{user.username}</p>
-                            <p>{user.email}</p>
-                        </div>
-                    )
-                })
-          }
-      </>
-  )
+    )
 }
 export default List;
 
